07be97e346d01eb467e83655957ae6e14b793d92
Leo wilis <leowilis9898@gmail.com>
2026-06-24 17:06:12 +0700

    feat(cart): define CartItem type data contract for basket orchestration

---
+++ src/features/cart/type.ts
@@ -0,0 +1,10 @@
+export type CartItem = {
+  menuId: number;
+  cartItemId?: number;
+  name: string;
+  price: number;
+  image: string;
+  restaurantId: number;
+  restaurantName: string;
+  qty: number;
+};
