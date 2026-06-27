a04239e086c886222db9c93caf3cc7c0c5c03165
Leo wilis <leowilis9898@gmail.com>
2026-06-27 14:12:19 +0700

    chore(main): wrap application root with Redux Provider and QueryClient context

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { Provider } from 'react-redux'
+import { router } from './app/router'
+import { store } from './app/store'
+import { queryClient } from './config/queryClient'
 import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </Provider>
+  </React.StrictMode>,
 )
