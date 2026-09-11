aa3fe54392eee91ab7ed4746e8dd72d162b3c185
Leo wilis <leowilis9898@gmail.com>
2026-09-11 14:58:18 +0700

    fix(router): correct details route path from 'detail' to 'details'

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,7 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom';
 import AuthPage from '@/features/auth/AuthPage';
 import HomePage from '@/features/home/HomePage';
-import ProtectedRoute from '@/components/layout/ProctectedRoute';
+import ProtectedRoute from '@/components/layout/ProtectedRoute';
+import RootLayout from '@/components/layout/RootLayout';
+import LazyWrapper from '@/components/layout/LazyWrapper';
+import {
+  DetailsPage,
+  CheckoutPage,
+  SuccessPage,
+  MyCartPage,
+} from './lazyRoutes';
 
 /**
  * Global application router configuration.
@@ -15,6 +23,45 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: <ProtectedRoute />,
-    children: [{ index: true, element: <HomePage /> }],
+    children: [
+      {
+        element: <RootLayout />,
+        children: [
+          { index: true, element: <HomePage /> },
+          {
+            path: 'details/:id',
+            element: (
+              <LazyWrapper>
+                <DetailsPage />
+              </LazyWrapper>
+            ),
+          },
+          {
+            path: 'checkout',
+            element: (
+              <LazyWrapper>
+                <CheckoutPage />
+              </LazyWrapper>
+            ),
+          },
+          {
+            path: 'success',
+            element: (
+              <LazyWrapper>
+                <SuccessPage />
+              </LazyWrapper>
+            ),
+          },
+          {
+            path: 'mycart',
+            element: (
+              <LazyWrapper>
+                <MyCartPage />
+              </LazyWrapper>
+            ),
+          },
+        ],
+      },
+    ],
   },
 ]);
