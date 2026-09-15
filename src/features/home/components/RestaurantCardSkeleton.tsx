14c816c0d944ca8ae433f95b4ad1eb38175954ee
Leo wilis <leowilis9898@gmail.com>
2026-09-15 21:42:13 +0700

    refactor(home): extract standalone RestaurantCardSkeleton placeholder component

--- src/features/home/components/RestaurantCardSkeleton.tsx
+++ src/features/home/components/RestaurantCardSkeleton.tsx
@@ -2,19 +2,12 @@ import { Skeleton } from '@/components/ui/skeleton';
 
 export default function RestaurantCardSkeleton() {
   return (
-    <div className='flex flex-col gap-3 p-4 border border-neutral-100 rounded-2xl w-full'>
-      {/* Restaurant Thumbnail */}
-      <Skeleton className='h-40 w-full rounded-xl' />
-
-      <div className='flex flex-col gap-2'>
-        {/* Restaurant Name */}
-        <Skeleton className='h-6 w-3/4 rounded' />
-
-        {/* Rating & Distance */}
-        <div className='flex gap-2'>
-          <Skeleton className='h-4 w-12 rounded' />
-          <Skeleton className='h-4 w-16 rounded' />
-        </div>
+    <div className='flex w-full flex-col overflow-hidden rounded-3xl border border-neutral-200 bg-white shadow-sm select-none animate-fadeIn'>
+      <Skeleton className='aspect-[4/3] w-full rounded-none' />
+      <div className='flex flex-col gap-2 p-4'>
+        <Skeleton className='h-5 w-3/4 rounded-md' />
+        <Skeleton className='h-4 w-1/3 rounded-md' />
+        <Skeleton className='h-4 w-2/3 rounded-md' />
       </div>
     </div>
   );
